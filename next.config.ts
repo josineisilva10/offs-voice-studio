cdc56ab4f6d0353febf124557e1d7a0c7f214965
INCOMPAR GAMER <josineisilva2@gmail.com>
2025-11-29 22:55:34 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
     ],
   },
 };
