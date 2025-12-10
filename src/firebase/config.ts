f2a5bdc962279ccb02f6b7505886198b389d99ca
INCOMPAR GAMER <josineisilva2@gmail.com>
2025-12-10 16:29:50 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:740572329898:web:0ef6319d1d71f1a7a95dd3",
   "apiKey": "AIzaSyDY1jkCNi5zodAnL-LIYL1gDMTRHQbIqEE",
   "authDomain": "studio-611847233-7c1f1.firebaseapp.com",
+  "storageBucket": "studio-611847233-7c1f1.appspot.com",
   "measurementId": "",
   "messagingSenderId": "740572329898"
 };
